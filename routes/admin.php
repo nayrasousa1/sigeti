d32467aed1d1136181cb9f588515a2350c2afa80
Nayra Geovana de Sousa Rodrigues <nararodrygues530@gmail.com>
2026-05-11 15:00:01 -0300

    feat: adicionar arquivo de rotas admin

--- routes/admin.php
+++ routes/admin.php
@@ -7,11 +7,32 @@
 */
 use CoffeeCode\Router\Router;
 
-$router = new Router(APP_URL, "@");
-$router->namespace("app\Controllers");
+$router->group(null);
+$router->group("/admin");
 
-$router->get("/dashboard", "Admin\DashboardController@index");
+$router->get("/dashboard", "admin\DashboardController@index");
 
+$router->get("/perfis", "admin\RoleController@index");
+$router->get("/perfis/cadastrar", "admin\RoleController@create");
+$router->post("/perfis/cadastrar", "admin\RoleController@store");
 
-$router->get("/perfil", "Admin\RoleController@index");
-$
+$router->get("/perfis/editar/{id}", "admin\RoleController@edit");
+$router->put("/perfis/editar/{id}", "admin\RoleController@update");
+$router->delete("/perfis/excluir/{id}", "admin\RoleController@destroy");
+
+$router->get("/perfis/{id}/permissoes", "admin\RolePermissionController@edit");
+$router->post("/perfis/{id}/permissoes", "admin\RolePermissionController@update");
+
+$router->get("usuarios", "admin\UserController@index");
+$router->get("usuarios/cadastrar", "admin\UserController@create");
+$router->post("/usuarios/cadastrar", "admin\UserController@store");
+$router->get("/usuarios/editar/{id}", "admin\UserController@edit");
+$router->put("/usuarios/editar/{id}", "admin\UserController@update");
+$router->delete("/usuarios/excluir/{id}", "admin\UserController@destroy");
+
+$router->get("/departamentos", "admin\DepartmentController@index");
+$router->get("/departamentos/cadastrar", "admin\DepartmentController@create");
+$router->post("/departamentos/cadastrar", "admin\DepartmentController@store");
+$router->get("/departamentos/editar/{id}", "admin\DepartmentController@edit");
+$router->put("/departamentos/editar/{id}", "admin\DepartmentController@update");
+$router->delete("/departamentos/excluir/{id}", "admin\DepartmentController@destroy");
